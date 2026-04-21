42993d1c8cbb03027b572f99a3af09e632e605d8
Karla Walker <indiablonix@gmail.com>
2026-04-20 22:42:58 -0700

    Added first scroll animation (drawSVG)

--- project-1/js/animations.js
+++ project-1/js/animations.js
@@ -1,4 +1,24 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   // register all plugins
-  gsap.registerPlugin();
+  gsap.registerPlugin(DrawSVGPlugin, ScrollTrigger, ScrollSmoother);
+
+  const smoother = ScrollSmoother.create({
+    wrapper: "#smooth-wrapper",
+    content: "#smooth-content",
+    smooth: 2,
+  });
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#pattern-1, #pattern-2",
+      scrub: true,
+      start: "clamp(top bottom-=300px)",
+      end: "clamp(bottom top)",
+    },
+  });
+
+  tl.from("#pattern-1, #pattern-2", {
+    duration: 3,
+    drawSVG: 0,
+  });
 });
