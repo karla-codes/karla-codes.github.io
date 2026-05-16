88f88d58afc35062b936300bd1dd4770b8c79ac9
Karla Walker <indiablonix@gmail.com>
2026-05-15 18:56:24 -0700

    Added test 3d object to scene

--- project-2/js/main.js
+++ project-2/js/main.js
@@ -1,1 +1,65 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+// Canvas
+const canvas = document.querySelector("#c");
+
+// Scene
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 3;
+scene.add(camera);
+
+// Orbit Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  // update renderer
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+// Materials
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  wireframe: true,
+});
+
+// Objects (Mesh)
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(), material);
+scene.add(sphere);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({ canvas });
+const width = window.innerWidth;
+const height = window.innerHeight;
+renderer.setSize(width, height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const animation = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // controls.update() required in animation function if controls.enableDamping or controls.autoRotate are set to true
+  controls.update();
+
+  // render scene inside animation() to update on each frame
+  renderer.render(scene, camera);
+
+  // add at end to run animation() on next frame
+  window.requestAnimationFrame(animation);
+};
+
+animation();
